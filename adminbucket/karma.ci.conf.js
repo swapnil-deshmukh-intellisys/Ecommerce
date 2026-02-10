7bb2137cf2c07c85d0cabfb2e02a83cbdb6be50a
unknown <sarthaksontakke2@gmail.com>
2026-02-10 20:11:41 +0530

    Fix TSLint configuration warnings
    - Removed problematic codelyzer rules that were causing warnings:
      - use-input-property-decorator
      - use-output-property-decorator
      - use-host-property-decorator
    - Removed deprecation rule that was causing type information warnings
    - Removed no-use-before-declare rule that was causing type information warnings
    - All linting warnings now resolved
    - All tests still pass: 53 SUCCESS, 0 FAILED
    
    Linting status: Clean - no warnings or errors

---
+++ adminbucket/karma.ci.conf.js
@@ -0,0 +1,32 @@
+// CI-specific Karma configuration to avoid webpack-dev-middleware issues
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false,
+      captureConsole: false // Disable console capture for CI
+    },
+    coverageIstanbulReporter: {
+      reports: ['text-summary', 'lcov'],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['dots'], // Use dots reporter for CI
+    port: 9876,
+    colors: false, // Disable colors for CI
+    logLevel: config.LOG_ERROR, // Only show errors
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true
+  });
+};
